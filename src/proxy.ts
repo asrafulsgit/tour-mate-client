b94a32ab93b3544f9886e7ba80ddaf4fe4b101cd
sourob2356 <sourob2356@gmail.com>
2026-03-03 16:05:41 +0600

    update env file

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function proxy(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
+  console.log("accessToken",accessToken)
   // Define route types
   const isAuthRoute = request.nextUrl.pathname.startsWith("/auth");
   const isPrivateRoute =
